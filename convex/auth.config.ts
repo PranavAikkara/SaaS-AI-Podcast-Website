bf6fa7ff5f29581787c2ce5563f0fcc1f521822c
PranavAikkara <aikkara.pranav@.com>
2024-08-07 21:52:45 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,13 @@
+//
+
+const authConfig  ={
+    providers: [
+      {
+        domain: "https://first-eel-96.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
+  
